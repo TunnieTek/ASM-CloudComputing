8dbe90c067f9d74d64d7bc104112a4ec3fc75dd0
Quach Cong Tuan <cnqtuannn@gmail.com>
2022-04-09 08:14:15 +0700

    Cloud Computing

---
+++ connectToMysql.php
@@ -0,0 +1,10 @@
+<?php
+// $GLOBALS['conn'] = mysqli_connect("localhost", "root", "", "toystore");
+$GLOBALS['conn'] = mysqli_connect("localhost", "root", "", "toystore");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+
